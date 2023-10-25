dc83b84139ca33608a3f36fe8e7801a843bf2951
kh.kim <kh.kim9700@gmail.com>
2023-10-25 20:40:38 +0900

    Revert "Merge branch 'KMT_CreatStartScene' into dev"
    This reverts commit 21d343c6d0e11d99681092861dabb4bb60b69cb2, reversing
    changes made to e0599aeeaae27f87849c58d43c32048689d2e59e.

--- Assets/Scripts/KMT/StartScene/SoundManager.cs
+++ Assets/Scripts/KMT/StartScene/SoundManager.cs
@@ -12,7 +12,6 @@ public class SoundManager : MonoBehaviour
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider sfxSlider;
-    [SerializeField] Toggle muteToggle;
     float master;
     float bgm;
     float sfx;
@@ -29,6 +28,7 @@ public class SoundManager : MonoBehaviour
     }
     private void Start()
     {
+        Debug.Log(PlayerPrefs.GetFloat("BGM"));
         master = PlayerPrefs.GetFloat("Master");
         masterMixer.SetFloat("Master", master);
         masterSlider.value = master;
@@ -38,8 +38,7 @@ public class SoundManager : MonoBehaviour
         sfx = PlayerPrefs.GetFloat("SFX");
         masterMixer.SetFloat("SFX", sfx);
         sfxSlider.value = sfx;
-        muteToggle.isOn = PlayerPrefs.GetInt("Mute") == 1;
-        MuteCheck(muteToggle.isOn);
+        
     }
     public void MasterControl()
     {
@@ -80,10 +79,14 @@ public class SoundManager : MonoBehaviour
         }
         PlayerPrefs.SetFloat("SFX", sfx);
     }
-    public void MuteCheck(bool isMute)
+    public void AudioOff()
     {
-        masterMixer.SetFloat("BGM", isMute? -80f : bgm);
-        masterMixer.SetFloat("SFX", isMute? -80f : sfx);
-        PlayerPrefs.SetInt("Mute", isMute ? 1 : 0);
+        masterMixer.SetFloat("BGM", -80);
+        masterMixer.SetFloat("SFX", -80);
+    }
+    public void AudioOn()
+    {
+        masterMixer.SetFloat("BGM", bgm);
+        masterMixer.SetFloat("SFX", sfx);
     }
 }
