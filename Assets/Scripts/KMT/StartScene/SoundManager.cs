321c6fe878d4fc263d68a1a485dff7f6b44cf3b1
rlaalsxo <alsxo124@naver.com>
2023-10-26 20:10:33 +0900

    [Update]StartSceneManager.cs,GameManager.cs(김민태)

--- Assets/Scripts/KMT/StartScene/SoundManager.cs
+++ Assets/Scripts/KMT/StartScene/SoundManager.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
-using UnityEngine.Timeline;
 
 public class SoundManager : MonoBehaviour
 {
