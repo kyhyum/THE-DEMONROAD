d8a1a3238a4cf1bd518f6c1d054f1210a43a478c
rlaalsxo <alsxo124@naver.com>
2023-10-27 20:38:05 +0900

    [Update]GameManager.cs, StartSceneManager.cs(김민태)

--- Assets/Scripts/KMT/StartScene/GameManager.cs
+++ Assets/Scripts/KMT/StartScene/GameManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public PlayerData playerData;
     [SerializeField] GameObject settingCanvas;
     public static GameManager s_instance;
+    public GameObject player;
     private void Awake()
     {
         if (s_instance == null)
@@ -33,14 +34,29 @@ public class GameManager : MonoBehaviour
         // 파일 생성 및 저장
         File.WriteAllText(path, jsonData);
     }
-    public PlayerData LoadPlayerDataFromJson(string jsonPath, string characterName, PlayerData data)
+    public PlayerData LoadPlayerDataFromJson(string jsonPath, string characterName)
     {
         // 데이터를 불러올 경로 지정
         string path = Path.Combine(jsonPath, $"{characterName}.json");
         // 파일의 텍스트를 string으로 저장
         string jsonData = File.ReadAllText(path);
         // 이 Json데이터를 역직렬화하여 playerData에 넣어줌
-        data = JsonUtility.FromJson<PlayerData>(jsonData);
-        return data;
+        return JsonUtility.FromJson<PlayerData>(jsonData);
+        
+    }
+    void Save()
+    {
+        PlayerData data = player.GetComponent<PlayerData_KMT>().playerData;
+        string prefabPath = "Assets/Resources/MyCharacter/";
+        SavePlayerDataToJson(prefabPath, data.name, data);
+    }
+    public void SceneLoad()
+    {
+        SceneManager.LoadScene(player.GetComponent<PlayerData_KMT>().playerData.sceneIndex);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void OnApplicationQuit()
+    {
+        Save();
     }
 }
