ce7ef0b289ff7084ae126aae73a66ea606e8f8cf
kyhyum <dudrhkd0225@naver.com>
2023-10-30 12:56:01 +0900

    [Update] Monster FSM 완성

---
+++ Assets/KYH_Test/KYH_Script/Monster/StateMachine/MonsterChaseState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MonsterChaseState : MonsterBaseState
+{
+    public MonsterChaseState(MonsterStateMachine monsterStateMachine) : base(monsterStateMachine)
+    {
+        stateMachine = monsterStateMachine;
+    }
+
+    public override void Enter()
+    {
+        stateMachine.MovementSpeedModifier = 1;
+        base.Enter();
+        StartAnimation(stateMachine.Monster.monsterAnimationData.ChaseParameterHash);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StopAnimation(stateMachine.Monster.monsterAnimationData.ChaseParameterHash);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (!IsInChaseRange())
+        {
+            stateMachine.ChangeState(stateMachine.IdleState);
+            return;
+        }
+        else if (IsInAttackRange())
+        {
+            stateMachine.ChangeState(stateMachine.AttackState);
+            return;
+        }
+    }
+    private bool IsInAttackRange()
+    {
+        // if (stateMachine.Target.IsDead) { return false; }
+
+        float playerDistanceSqr = (stateMachine.Target.transform.position - stateMachine.Monster.transform.position).sqrMagnitude;
+
+        return playerDistanceSqr <= stateMachine.Monster.Data.AttackRange * stateMachine.Monster.Data.AttackRange;
+    }
+}
---
+++ Assets/KYH_Test/KYH_ScriptableObjects/Scripts/MonsterSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterSO", menuName = "Characters/Monster")]
+public class MonsterSO : ScriptableObject
+{
+    [field: Header("AttackData")]
+    [field: SerializeField] public bool IsLongRanged { get; private set; } = false;
+    [field: SerializeField] public float PlayerChasingRange { get; private set; } = 10f;
+    [field: SerializeField] public float AttackRange { get; private set; } = 1.5f;
+    [field: SerializeField][field: Range(0f, 3f)] public float ForceTransitionTime { get; private set; }
+    [field: SerializeField][field: Range(-10f, 10f)] public float Force { get; private set; }
+    [field: SerializeField] public int Damage { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_Start_TransitionTime { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_End_TransitionTime { get; private set; }
+
+
+    [field: Header("MoveData")]
+    [field: SerializeField][field: Range(0f, 25f)] public float BaseSpeed { get; private set; } = 5f;
+    [field: SerializeField][field: Range(0f, 25f)] public float BaseRotationDamping { get; private set; } = 1f;
+
+    [field: SerializeField][field: Range(0f, 2f)] public float RunSpeedModifier { get; private set; } = 1f;
+
+}
