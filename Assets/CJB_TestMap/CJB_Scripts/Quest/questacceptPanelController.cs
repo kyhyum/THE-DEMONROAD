0dfdb5f10156fd8dde8b7335383ac11a904c00b4
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-31 18:01:47 +0900

    [ADD/UPDATE] questbtn 프리팹화, 스크립트 수정
    - 각각의 버튼을 만들어서 퀘스트목록을 누르고 각자 업데이트 되어 반영되게끔 했습니다.

---
+++ Assets/CJB_TestMap/CJB_Scripts/Quest/questacceptPanelController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class questacceptPanelController : MonoBehaviour
+{
+    public GameObject questacceptPanel; 
+
+    
+    public bool isUIVisible = false;
+
+    void Start()
+    {
+        questacceptPanel.SetActive(false);
+    }
+
+    public void ToggleQuestPanelVisibility()
+    {
+        isUIVisible = !isUIVisible;
+        questacceptPanel.SetActive(isUIVisible);
+    }
+}
