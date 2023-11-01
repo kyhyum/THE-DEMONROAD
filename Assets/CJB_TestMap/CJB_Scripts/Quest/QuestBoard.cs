bfc9a9621a27b798e737560c931ad10b96244f83
JangbeomChoi <jangbeom0325@gmail.com>
2023-11-01 17:06:28 +0900

    [UPDATE] 스크립트 수정

--- Assets/CJB_TestMap/CJB_Scripts/Quest/QuestBoard.cs
+++ Assets/CJB_TestMap/CJB_Scripts/Quest/QuestBoard.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,12 +10,15 @@ public class QuestBoard : MonoBehaviour
     public GameObject questListPanel;
     public GameObject questDetailsPanel;
     public GameObject questButton;
+
     public TMP_Text questTitleText;
     public TMP_Text questacceptNameText;
     public TMP_Text questDescriptionText;
     public TMP_Text questConditionText;
     public TMP_Text questRewardText;
+
     public List<QuestSO> quests;
+    public List<QuestSO> acceptedQuests = new List<QuestSO>();
 
     public void Start()
     {
@@ -44,11 +48,34 @@ public class QuestBoard : MonoBehaviour
     }
     private void ShowQuestDetails(QuestSO selectedQuest)
     {
+        if (IsQuestAlreadyAccepted(selectedQuest))
+        {
+            return;
+        }
+        AcceptQuest(selectedQuest);
+
         questDetailsPanel.SetActive(true);
         questacceptNameText.text = selectedQuest.questName;
         questTitleText.text = selectedQuest.questName;
         questDescriptionText.text = selectedQuest.questDescription;
         questConditionText.text = selectedQuest.questCondition;
         questRewardText.text = selectedQuest.questReward;
+    }
+
+    private bool IsQuestAlreadyAccepted(QuestSO quest)
+    {
+        foreach (var acceptedQuest in acceptedQuests)
+        {
+            if (acceptedQuest.questType == quest.questType) // questType는 각 퀘스트의 종류를 나타내는 변수
+            {
+                return true; 
+            }
+        }
+        return false;
+    }
+
+    private void AcceptQuest(QuestSO quest)
+    {
+        acceptedQuests.Add(quest);
     }
 }
