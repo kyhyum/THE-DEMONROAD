29faa3e3b4862a50817afe7bcc55289d10f7a7e1
rlaalsxo <alsxo124@naver.com>
2023-11-02 17:31:47 +0900

    [Update]StartScene

--- Assets/Scripts/KMT/StartScene/PlayerCondition.cs
+++ Assets/Scripts/KMT/StartScene/PlayerCondition.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 
 public enum Job
 {
@@ -49,8 +51,43 @@ public class Stat
 public class PlayerCondition : MonoBehaviour
 {
     public PlayerData playerData;
+    StatType mainStat;
+    public float atk;
+    public float def;
+    public float speed;
+    public float currentHp;
+    public float maxHp;
+    public float currentMp;
+    public float maxMp;
+    Dictionary<StatType, int> myStats;
     private void Start()
     {
-        Debug.Log(playerData.name);
+        switch (playerData.job)
+        {
+            case Job.WARRIOR:
+                mainStat = StatType.STR;
+                break;
+            case Job.ARCHOR:
+                mainStat = StatType.DEX;
+                break;
+            case Job.WIZZARD:
+                mainStat = StatType.INT;
+                break;
+        }
+        for(int i = 0; i < playerData.stats.Count; i++)
+        {
+            myStats.Add(playerData.stats[i].type, playerData.stats[i].statValue);
+        }
+        atk = myStats[mainStat] * 3;
+        def = myStats[StatType.DEX] * 0.5f;
+        speed = myStats[StatType.DEX] + 10;
+        maxHp = myStats[StatType.CON] + myStats[StatType.STR] * 5;
+        currentHp = maxHp;
+        maxMp = myStats[StatType.INT] * 10;
+        currentMp = maxMp;
+        for(int i = 0; i < playerData.stats.Count; i++)
+        {
+            playerData.stats[i].statValue = myStats[playerData.stats[i].type];
+        }
     }
 }
