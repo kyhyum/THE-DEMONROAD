17496d406208ef7b974192407fef867825088e57
kh.kim <kh.kim9700@gmail.com>
2023-11-06 17:44:06 +0900

    [CHORE] 이름변경
    [CHORE] InventorySlot -> ItemSlot

--- Assets/Scripts/KH/UI/ItemSlot.cs
+++ Assets/Scripts/KH/UI/ItemSlot.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour
 {
     public int slotID;
     public bool isContain;
