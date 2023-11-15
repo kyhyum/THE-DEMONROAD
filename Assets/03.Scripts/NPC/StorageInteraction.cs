0d85bcae7f512b3fc5869ea426ff0c8dd5f66c5d
JangbeomChoi <jangbeom0325@gmail.com>
2023-11-15 19:37:40 +0900

    [ADD/UPDATE] storageui추가, 스크립트 수정
    [ADD/UPDATE] storageui추가, 스크립트 수정

--- Assets/03.Scripts/NPC/StorageInteraction.cs
+++ Assets/03.Scripts/NPC/StorageInteraction.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StorageInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform player;
+    public GameObject interactionPopup;
+    public GameObject storageUI;
+    private bool isUIVisible = false;
+
+    public float activationDistance = 5f;
     void Start()
     {
-        
+        player = GameManager.Instance.Myplayer.transform;
+        storageUI.SetActive(false);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance <= activationDistance)
+        {
+            interactionPopup.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                isUIVisible = !isUIVisible;               
+                storageUI.SetActive(true);
+                
+            }
+        }
+        else
+        {
+            storageUI.SetActive(false);
+            interactionPopup.SetActive(false);
+        }
     }
 }
+
