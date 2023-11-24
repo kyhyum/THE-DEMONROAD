6cb29a0054db1ba7a0bd6d19f454cdb0d2a27fd9
KHS <kij4300@naver.com>
2023-11-24 15:15:36 +0900

    [TEST] 검 이펙트
    이펙트 위치 설정하고 이펙트 나가게 했다.

--- Assets/Scripts/Player/FSM/Characters/Player/StateMachines/AttackStates/PlayerAttackState.cs
+++ Assets/Scripts/Player/FSM/Characters/Player/StateMachines/AttackStates/PlayerAttackState.cs
@@ -12,10 +12,11 @@ public class PlayerAttackState : PlayerBaseState
     {
         Debug.Log("PlayerAttackState 클래스 Enter 함수 호출한다.");
 
+        Player player = stateMachine.Player;
         Weapon weapon = stateMachine.Player.Weapon;
 
         //stateMachine.Player.AttackSlash
-        Object.Instantiate(weapon.AttackSlash, weapon.transform.position, Quaternion.identity);
+        Object.Instantiate(weapon.AttackSlash, player.AttackVfxPoint.transform.position, Quaternion.identity);
         
         stateMachine.Player.Agent.ResetPath();
         base.Enter();
@@ -34,15 +35,17 @@ public class PlayerAttackState : PlayerBaseState
     {
         base.Update();
 
+        Debug.Log("PlayerAttackState 클래스 Update 함수 호출한다.");
+
         float normalizedTime = GetNormalizedTime(stateMachine.Player.Animator, "Attack");
         if (normalizedTime < 0.8f)
         {
             if (stateMachine.Player.IsAttacking)
             {
-                Debug.Log("PlayerAttackState 클래스 Update 함수 호출한다.");
+                Debug.Log("PlayerAttackState 클래스 Update 함수 호출2한다.");
             }
 
-            Debug.Log("PlayerAttackState 클래스 Update2 함수 호출한다. 2");
+            Debug.Log("PlayerAttackState 클래스 Update 함수 호출3한다.");
         }
         else
         {
