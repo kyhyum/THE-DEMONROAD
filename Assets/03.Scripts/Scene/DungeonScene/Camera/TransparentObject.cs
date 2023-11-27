6c5b8766176afbd1052081b34747360973c366ee
kyhyum <dudrhkd0225@naver.com>
2023-11-28 01:04:19 +0900

    [Update] Transperent.cs

--- Assets/03.Scripts/Scene/DungeonScene/Camera/TransparentObject.cs
+++ Assets/03.Scripts/Scene/DungeonScene/Camera/TransparentObject.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TransparentObject : MonoBehaviour
 {
-    public Material TransparentMaterial;
-
     private Renderer meshRenderer;
+
+    //타운맵
+    private Material[] TransparentMaterials;
+    private Material[] originalMaterials;
+
+    //던전맵
+    public Material TransparentMaterial;
     private Material originalMaterial;
 
+    /*------------*/
     float elapsedTime = 0f;
     private bool isTransparent = false;
 
@@ -17,25 +26,99 @@ public class TransparentObject : MonoBehaviour
 
     void Start()
     {
+        
         meshRenderer = GetComponent<MeshRenderer>();
-        originalMaterial = meshRenderer.material;
+        if (TransparentMaterial == null)
+        {
+            originalMaterials = meshRenderer.materials;
+            TransparentMaterials = new Material[originalMaterials.Length];
+            for(int i = 0; i < originalMaterials.Length; i++)
+            {
+                TransparentMaterials[i] = new Material(originalMaterials[i]);
+                TransparentMaterials[i].SetFloat("_Surface", 1.0f);
+                TransparentMaterials[i].SetFloat("_Cull", (float)CullMode.Off);
+                TransparentMaterials[i].color = new Color(1, 1, 1, 0.3f);
+            }
+
+        }
+        else
+        {
+            originalMaterial = meshRenderer.material;
+
+        }
     }
 
     public void BecomeTransparency()
     {
         if(TransparentMaterial == null)
         {
-            // 다른 것
+            SetTransparent();
         }
         else
         {
-            SetTransparent();
+            SetTransparentMaterial();
         }
     }
 
+    //타운맵에서 사용//
     public void SetTransparent()
     {
+        float lerpValue;
         setTransparentCoroutine = StartCoroutine(SetUnTransparent());
+        if (isTransparent)
+        {
+            StopCoroutine(setTransparentCoroutine); // Stop the previous coroutine
+            return;
+        }
+        while (elapsedTime <= 1.0f)
+        {
+            lerpValue = Mathf.Lerp(0f, 1f, elapsedTime / 1.0f);
+
+            for (int i = 0; i < originalMaterials.Length; i++)
+            {
+                meshRenderer.materials[i].Lerp(originalMaterials[i], TransparentMaterials[i], lerpValue);
+            }
+            // 경과 시간 업데이트
+            elapsedTime += Time.deltaTime;
+        }
+
+        for (int i = 0; i < originalMaterials.Length; i++)
+        {
+            meshRenderer.materials[i] = TransparentMaterials[i];
+        }
+        isTransparent = true;
+    }
+
+    IEnumerator SetUnTransparent()
+    {
+        yield return new WaitForSeconds(1f);
+        float lerpValue;
+        while (elapsedTime <= 1.0f)
+        {
+            lerpValue = Mathf.Lerp(0f, 1f, elapsedTime / 1.0f);
+            for (int i = 0; i < originalMaterials.Length; i++)
+            {
+                meshRenderer.materials[i].Lerp(TransparentMaterials[i], originalMaterials[i], lerpValue);
+            }
+            // 경과 시간 업데이트
+            elapsedTime += Time.deltaTime;
+        }
+        
+        for (int i = 0; i < originalMaterials.Length; i++)
+        {
+            meshRenderer.materials[i] = originalMaterials[i];
+        }
+
+        isTransparent = false;
+    }
+
+
+
+
+    // 던전에서 사용//
+    public void SetTransparentMaterial()
+    {
+        setTransparentCoroutine = StartCoroutine(SetUnTransparentMaterial());
         if (isTransparent)
         {
             StopCoroutine(setTransparentCoroutine); // Stop the previous coroutine
@@ -53,7 +136,7 @@ public class TransparentObject : MonoBehaviour
         isTransparent = true;
     }
 
-    IEnumerator SetUnTransparent()
+    IEnumerator SetUnTransparentMaterial()
     {
         yield return new WaitForSeconds(1f);
         float lerpValue;
