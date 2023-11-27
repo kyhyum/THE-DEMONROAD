e9266208c65429d35048e59a9c9196cc078ccf10
KHS <kij4300@naver.com>
2023-11-27 13:10:12 +0900

    [TEST] 스크립터블 오브젝트로 스킬 만들기
    스크립터블 오브젝트로 스킬을 만들어서 출력하게 했다.

--- Assets/03.Scripts/Skill/SkillSO.cs
+++ Assets/03.Scripts/Skill/SkillSO.cs
@@ -8,5 +8,11 @@ public class SkillSO : ScriptableObject
     [field: SerializeField] public string skillName;
     //[field: SerializeField] public Sprite icon;
     [field: SerializeField] public string description;
-    [field: SerializeField] public float ManaCost;
+    [field: SerializeField] public int damage;
+    [field: SerializeField] public int increasedDamagePerLevel;
+    [field: SerializeField] public float stunTime;
+    [field: SerializeField] public float increasedStunTimePerLevel;
+    [field: SerializeField] public int manaCost;
+    [field: SerializeField] public float coolTime;
+   
 }
