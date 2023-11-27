1b871a94902643eae252cd2e4a1f51f73fd0d133
kh.kim <kh.kim9700@gmail.com>
2023-11-27 15:00:46 +0900

    [CHORE] 사용하지 않는 스크립트 삭제

--- Assets/03.Scripts/Common/Managers/UIManager.cs
+++ Assets/03.Scripts/Common/Managers/UIManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
--- Assets/03.Scripts/UI/SkillSlot.cs
+++ Assets/03.Scripts/UI/SkillSlot.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class SkillSlot : MonoBehaviour
 {
-    
+    private Skill skill;
 }
