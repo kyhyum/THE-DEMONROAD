6510fa9a763ae5e6923d3ca3852cf01ad7edc661
kh.kim <kh.kim9700@gmail.com>
2023-11-29 15:31:14 +0900

    [UPDATE] 스킬 UI 수정
    [UPDATE] 스킬을 퀵슬롯으로 드래그앤드롭 기능 추가

--- Assets/03.Scripts/UI/SkillSlot.cs
+++ Assets/03.Scripts/UI/SkillSlot.cs
@@ -1,8 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillSlot : MonoBehaviour
 {
     private Skill skill;
+    public Image icon;
+    public TMP_Text nameTxt;
+    public TMP_Text descriptionTxt;
+    public TMP_Text levelTxt;
+
+    public Button addBtn;
+    public Button subBtn;
+
+    public void Set(Skill skill)
+    {
+        this.skill = skill;
+
+        nameTxt.text = skill.skillName;
+        descriptionTxt.text = skill.description;
+        levelTxt.text = string.Format("{0} / 5", skill.level);
+
+        if (skill.level == 0)
+        {
+            subBtn.interactable = false;
+        }
+
+        if (skill.level == 5)
+        {
+            addBtn.interactable = false;
+        }
+    }
+
+    public Skill GetSkill()
+    {
+        return skill;
+    }
+
+    public void ClickAddBtn()
+    {
+        subBtn.interactable = true;
+
+        skill.LevelUp();
+
+        if (skill.level == 5)
+        {
+            addBtn.interactable = false;
+        }
+    }
+
+    public void ClickSubBtn()
+    {
+        addBtn.interactable = true;
+
+        skill.LevelDown();
+
+        if (skill.level == 0)
+        {
+            subBtn.interactable = false;
+        }
+    }
 }
