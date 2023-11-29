3d661b2a4dbf6653d067a502a3fff224c70b6722
JangbeomChoi <jangbeom0325@gmail.com>
2023-11-29 16:26:19 +0900

    [UPDATE] 씬수정, 스크립트 수정 (-장범)
    -shop 갯수별로 사게끔 수정

--- Assets/03.Scripts/Shop/ShopManager.cs
+++ Assets/03.Scripts/Shop/ShopManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class ShopManager : MonoBehaviour
 {
@@ -13,9 +14,14 @@ public class ShopManager : MonoBehaviour
     public GameObject outofGoldPop;
 
     public TMP_Text confirmationText;
+    public TMP_Text itemCountText;
+
     public Button confirmationButton;
+    public Button increaseButton; 
+    public Button decreaseButton;
 
     private int itemCountToBuy = 1;
+    
     ItemSO item;
     
     
@@ -25,7 +31,9 @@ public class ShopManager : MonoBehaviour
         potionShop.SetActive(false);
         confirmationPopUp.SetActive(false);
         outofGoldPop.SetActive(false);
-        
+
+        increaseButton.onClick.AddListener(IncreaseItemCount);
+        decreaseButton.onClick.AddListener(DecreaseItemCount);
     }
 
     private void Update()
@@ -40,10 +48,11 @@ public class ShopManager : MonoBehaviour
     {
         item = clickedItem;
 
-        if(item != null)
+        if (item != null)
         {
             confirmationPopUp.SetActive(true);
-            confirmationText.text = "구매하시겠습니까? "+"\n" + item.itemName + " - " + item.itemPrice + " Gold";
+            confirmationText.text = "구매하시겠습니까? " + "\n" + item.itemName; 
+                                     
 
             confirmationButton.onClick.AddListener(BuyItem);
 
@@ -58,17 +67,21 @@ public class ShopManager : MonoBehaviour
     {
         Inventory inventory = UIManager.Instance.GetInventory();
 
-        if (inventory != null)
+        if (inventory != null && item != null)
         {
-            if (inventory.Gold >= item.itemPrice)
+            int totalPrice = item.itemPrice * itemCountToBuy;
+
+            if (inventory.Gold >= totalPrice)
             {
-                // 인벤토리에 아이템 추가
-                ItemSO itemSO = Resources.Load<ItemSO>(item.itemName);
-                Item itemToAdd = new Item(itemSO);
-                inventory.AddItem(itemToAdd);
+                for (int i = 0; i < itemCountToBuy; i++)
+                {
+                    ItemSO itemSO = Resources.Load<ItemSO>(item.itemName);
+                    Item itemToAdd = new Item(itemSO);
+                    inventory.AddItem(itemToAdd);
+                }
 
                 // 골드 차감
-                inventory.Gold -= item.itemPrice;
+                inventory.Gold -= totalPrice;
             }
             else
             {
@@ -82,6 +95,30 @@ public class ShopManager : MonoBehaviour
         }
         confirmationPopUp.SetActive(false);
     }
+    public void IncreaseItemCount()
+    {
+        // 아이템 갯수를 증가시키는 함수
+        itemCountToBuy++;
+        UpdateItemCount();
+    }
+    public void DecreaseItemCount()
+    {
+        // 아이템 갯수를 감소시키는 함수
+        itemCountToBuy = Mathf.Max(itemCountToBuy - 1, 1);
+        UpdateItemCount();
+    }
+
+    private void UpdateItemCount()
+    {
+        if(itemCountText != null)
+        {
+            itemCountText.text = itemCountToBuy.ToString() + " 개";
+            
+        }
+       
+    }
+
+
     public void ClosePop()
     {
         outofGoldPop.SetActive(false);
