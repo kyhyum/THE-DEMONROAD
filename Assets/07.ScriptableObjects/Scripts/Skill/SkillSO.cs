19df319b07e9ca7482dcb79556fc504d26b75858
kh.kim <kh.kim9700@gmail.com>
2023-11-30 18:01:07 +0900

    [UPDATE] 퀵슬롯 스킬 쿨타임 수정
    [UPDATE] 퀵슬롯에 같은 스킬을 여러개 등록후 사용시 각자 쿨타임이 도는 현상 수정

--- Assets/07.ScriptableObjects/Scripts/Skill/SkillSO.cs
+++ Assets/07.ScriptableObjects/Scripts/Skill/SkillSO.cs
@@ -9,5 +9,5 @@ public class SkillSO : ScriptableObject
     [field: SerializeField] public Sprite icon;
     [field: SerializeField] public SkillType type;
     [field: SerializeField] public int manaCost;
-    [field: SerializeField] public float coolTime;
+    [field: SerializeField] public float cooltime;
 }
