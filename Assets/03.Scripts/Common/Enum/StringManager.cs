d796fa674ea7d021a6c02d0a9c0a0f581c34ebf1
rlaalsxo <alsxo124@naver.com>
2023-11-30 18:34:36 +0900

    [Update]QuestLog창 QuestProgress창 UIManager 병합
    현재QuestLog랑 QuestProgress스크립트랑 프리팹이 없어서 애러가 날 수 있음

--- Assets/03.Scripts/Common/Enum/StringManager.cs
+++ Assets/03.Scripts/Common/Enum/StringManager.cs
@@ -9,9 +9,13 @@ public static class StringManager
     private static string testJsonPath = "Assets/Resources/Tester/";
     private static string itemJsonPath = "Assets/Resources/MyInventory/";
     private static string storageName = "Storage";
-    private static string storagePrefab = "Storage";
     private static string testItemJsonPath = "Assets/Resources/TestInventory/";
     private static string testStorageName = "TestStorage";
+    private static string inventoryPrefabPath = "Prefabs/UI/UI_Inventory";
+    private static string stroagePrefabPath = "Prefabs/UI/UI_Storage";
+    private static string skillPrefabPath = "Prefabs/UI/UI_SKill";
+    private static string questLogPrefabPath = "Prefabs/UI/UI_QuestLog";
+    private static string questProgressPath = "Prefabs/UI/UI_QuestProcess";
     public static string JsonPath { get { return jsonPath; } }
     public static string ItemJsonPath { get { return itemJsonPath; } }
     public static string StorageName { get { return storageName; } }
@@ -19,4 +23,9 @@ public static class StringManager
     public static string TestItemJsonPath { get { return testItemJsonPath; } }
     public static string TestStorageName { get { return testStorageName; } }
     public static string BaseObjectPath { get { return baseObjcetPath; } }
+    public static string InventoryPrefabPath { get { return inventoryPrefabPath; } }
+    public static string StroagePrefabPath { get { return stroagePrefabPath;} }
+    public static string SKillPrefabPath { get { return skillPrefabPath; } }
+    public static string QuestLogPrefabPath { get { return questLogPrefabPath; } }
+    public static string QuestProgressPath { get { return questProgressPath; } }
 }
