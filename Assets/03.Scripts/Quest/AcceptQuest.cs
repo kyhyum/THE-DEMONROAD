c4a96de2e551de4b33c0666a8a406461333e5d9c
rlaalsxo <alsxo124@naver.com>
2023-12-01 16:54:44 +0900

    Update AcceptQuest.cs

--- Assets/03.Scripts/Quest/AcceptQuest.cs
+++ Assets/03.Scripts/Quest/AcceptQuest.cs
@@ -7,7 +7,6 @@ public class AcceptQuest : MonoBehaviour
 {
     public TMP_Text questName;
     public QuestSO questSO;
-
     public void OnQuestObjectClick()
     {
         UIManager.Instance.GetQuestLog().ShowLogQuestDetails(questSO);
