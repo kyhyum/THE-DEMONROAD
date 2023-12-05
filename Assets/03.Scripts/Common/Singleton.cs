29994ce9cc9a1bc4a7a18d78b2c12f3f39779007
kh.kim <kh.kim9700@gmail.com>
2023-12-05 10:17:43 +0900

    [UPDATE] 싱글톤 수정
    [UPDATE] 싱글톤 수정

--- Assets/03.Scripts/Common/Singleton.cs
+++ Assets/03.Scripts/Common/Singleton.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T _instance;
-
-    public static T Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<T>();
-
-                if (_instance == null)
-                {
-                    string componentName = typeof(T).ToString();
-
-                    GameObject findObject = GameObject.Find(componentName);
-
-                    if (findObject == null)
-                    {
-                        findObject = new GameObject(componentName);
-                    }
-
-                    _instance = findObject.AddComponent<T>();
-                }
-
-                DontDestroyOnLoad(_instance);
-            }
-
-            return _instance;
-        }
-    }
+    public static T Instance;
 
     private void Awake()
     {
+        if (Instance == null)
+        {
+            FindObjectOfType<T>();
+        }
+
         if (Instance != this)
         {
             Destroy(gameObject);
